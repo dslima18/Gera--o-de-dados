39a0f1b1e357698a707f1b7cca73034f181cce27
Donato Souza <donatosouza33@gmail.com>
2024-01-15 15:08:22 -0300

    Introduzindo a classe RandomWalk para tratar de passeios aleatórios.

---
+++ random_walk.py
@@ -0,0 +1,9 @@
+from random import choice
+class RandomWalk():
+    """Uma classe para gerar passeios aleatórios."""
+    def __init__(self, num_points = 5000):
+        """Inicializa os atributos de um passeio."""
+        self.num_points = num_points
+        # Todos os passeios começam em (0, 0)
+        self.x_values = [0]
+        self.y_values = [0]
