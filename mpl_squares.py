7052bc69528c719ffa30ce12a8e64f2c323f6383
Donato Souza <donatosouza33@gmail.com>
2024-01-15 11:13:08 -0300

    Alterando o tipo do rótulo a espessura do gráfico.

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
 
 squares = [1, 4 , 9, 16, 25]
-plt.plot(squares)
+plt.plot(squares, linewidth=5)
+# Define o título do grafico e nomeia os eixos
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize = 14)
+plt.ylabel("Square os Value", fontsize=14)
+# Define o tamanho dos rótulos das marcações
+plt.tick_params(axis='both', labelsize = 14)
 plt.show()
