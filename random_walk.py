c2d51378d0516aa47fb44bff0e81550fcca18999
Donato Souza <donatosouza33@gmail.com>
2024-01-17 13:49:10 -0300

    Exercícios

--- random_walk.py
+++ random_walk.py
@@ -12,18 +12,20 @@ class RandomWalk():
         """Calcula todos os pontos do passeio."""
         # Continua dando passos até que o passeio alcance o tamanho desejado.
         while len(self.x_values) < self.num_points:
-            # Decide a direção a ser seguida e distância a ser percorrida nessa direção
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
             # Rejeita movimentos que não vão a lugar nenhum
-            if x_step == 0 and y_step ==0:
+            if x_step == 0 and y_step == 0:
                 continue
             # Calcula os próximos valores de x e de y
-            next_x = self.x_values[-1] +  x_step
+            next_x = self.x_values[-1] + x_step
             next_y = self.y_values[-1] + y_step
+
             self.x_values.append(next_x)
             self.y_values.append(next_y)
+    
+    def get_step(self):
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2 , 3, 4, 5, 6, 7, 8])
+        step = direction * distance
+        return step
