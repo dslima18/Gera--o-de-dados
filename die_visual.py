3a89a43ac0305ef8fa18114708347c83facd26a8
Donato Souza <donatosouza33@gmail.com>
2024-01-17 14:19:56 -0300

    Exercícios.

--- die_visual.py
+++ die_visual.py
@@ -3,24 +3,25 @@ import pygal
 # Cria um D6
 die_1 = Die()
 die_2 = Die()
+die_3 = Die()
 
 # Faz alguns lançamentos e armazena os resultados em uma lista
 results = []
-for roll_num in range(1000):
-    result = die_1.roll() + die_2.roll()
+for roll_num in range(4000):
+    result = die_1.roll() * die_2.roll() * die_3.roll()
     results.append(result)
 # Analisa os resultados.
 frequencies = []
-max_results = die_1.num_sides + die_2.num_sides
-for value in range(2, max_results+1):
+max_results = die_1.num_sides * die_2.num_sides * die_3.num_sides
+for value in range(1, max_results+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 # Visualiza os resultados
 hist = pygal.Bar()
-hist.tilte = "Results of rolling two D6 1000 times."
-hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.tilte = "Results of rolling three D6 1000 times."
+hist.x_labels = [x for x in range(1,216)]
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
-hist.add('D6 + D6', frequencies)
+hist.add('D6 * D6 * D6', frequencies)
 hist.render_to_file('die_visual.svg')
 #print(frequencies)
