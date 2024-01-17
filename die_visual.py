fa0ac769db0399c73073652526d52ddfa406223a
Donato Souza <donatosouza33@gmail.com>
2024-01-17 14:01:20 -0300

    Criando histogramas.

--- die_visual.py
+++ die_visual.py
@@ -1,9 +1,24 @@
 from die import Die
+import pygal
 # Cria um D6
 die = Die()
+
 # Faz alguns lançamentos e armazena os resultados em uma lista
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
-print(results)
+# Analisa os resultados.
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+# Visualiza os resultados
+hist = pygal.Bar()
+hist.tilte = "Results of rolling one D6 1000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
+#print(frequencies)
