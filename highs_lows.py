a07638c7054b6f61ba1b43b2f1a2f64231181e8d
Donato Souza <donatosouza33@gmail.com>
2024-01-18 19:48:31 -0300

    Plotando um gráfico de temperaturas

--- highs_lows.py
+++ highs_lows.py
@@ -1,4 +1,5 @@
 import csv
+from matplotlib import pyplot as plt
 # Obtém as temperaturas máximas do arquivo
 filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
@@ -9,3 +10,12 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
     print(highs)
+    # Faz a plotagem dos dados
+    fig = plt.figure(dpi=128, figsize=(10,6))
+    plt.plot(highs,c='red')
+    # Formata o gráfico
+    plt.title('Daily high temperatures, July 2014', fontsize = 24)
+    plt.xlabel('', fontsize = 16)
+    plt.ylabel('Temprature (F)',fontsize=16)
+    plt.tick_params(axis='both', which='major',labelsize=16)
+    plt.show()
