c96eac0924d8d3459430c828d4e2f08d7f21e311
Donato Souza <donatosouza33@gmail.com>
2024-01-19 13:58:36 -0300

    Adicionando as temperaturas mínimas.

--- highs_lows.py
+++ highs_lows.py
@@ -6,18 +6,21 @@ filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
         high = int(row[1])
         highs.append(high)
-    print(highs)
+        low = int(row[3])
+        lows.append(low)
+    #print(highs)
     # Faz a plotagem dos dados
     fig = plt.figure(dpi=128, figsize=(10,6))
+    plt.plot(dates, lows, c='blue')
     plt.plot(dates, highs,c='red')
     # Formata o gráfico
-    plt.title('Daily high temperatures - 2014', fontsize = 24)
+    plt.title('Daily high and low temperatures - 2014', fontsize = 24)
     plt.xlabel('', fontsize = 16)
     fig.autofmt_xdate()
     plt.ylabel('Temprature (F)',fontsize=16)
