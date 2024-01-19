79ad793a3bc0354829df4605cfa008cfb16cfc05
Donato Souza <donatosouza33@gmail.com>
2024-01-19 14:02:06 -0300

    Adicionando sombreamento a uma área do gráfico.

--- highs_lows.py
+++ highs_lows.py
@@ -17,8 +17,9 @@ with open(filename) as f:
     #print(highs)
     # Faz a plotagem dos dados
     fig = plt.figure(dpi=128, figsize=(10,6))
-    plt.plot(dates, lows, c='blue')
-    plt.plot(dates, highs,c='red')
+    plt.plot(dates, highs,c='red', alpha=0.5)
+    plt.plot(dates, lows, c='blue',alpha=0.5)
+    plt.fill_between(dates, highs, lows, facecolor='green', alpha=0.2)
     # Formata o gráfico
     plt.title('Daily high and low temperatures - 2014', fontsize = 24)
     plt.xlabel('', fontsize = 16)
