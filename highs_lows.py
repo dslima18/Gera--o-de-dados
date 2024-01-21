0c60f6522adbdd04533d8239c998e608e64d0634
Donato Souza <donatosouza33@gmail.com>
2024-01-21 11:05:30 -0300

    Extraindo dados relevantes e exercícios.

--- highs_lows.py
+++ highs_lows.py
@@ -2,18 +2,23 @@ import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 # Obtém as temperaturas máximas do arquivo
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     dates, highs, lows = [], [], []
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-        high = int(row[1])
-        highs.append(high)
-        low = int(row[3])
-        lows.append(low)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date,'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+
     #print(highs)
     # Faz a plotagem dos dados
     fig = plt.figure(dpi=128, figsize=(10,6))
@@ -21,9 +26,46 @@ with open(filename) as f:
     plt.plot(dates, lows, c='blue',alpha=0.5)
     plt.fill_between(dates, highs, lows, facecolor='green', alpha=0.2)
     # Formata o gráfico
-    plt.title('Daily high and low temperatures - 2014', fontsize = 24)
+    title = 'Daily high and low temperatures - 2014\nDeath Valley, CA'
+    plt.title(title, fontsize=20)
     plt.xlabel('', fontsize = 16)
     fig.autofmt_xdate()
     plt.ylabel('Temprature (F)',fontsize=16)
     plt.tick_params(axis='both', which='major',labelsize=16)
+    plt.ylim(0,100)
+    plt.savefig('death_valley_2014.png', bbox_inches='tight')
+    plt.show()
+
+filename = 'sitka_weather_2014.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    dates, highs, lows = [], [], []
+    for row in reader:
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date,'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+
+    #print(highs)
+    # Faz a plotagem dos dados
+    fig = plt.figure(dpi=128, figsize=(10,6))
+    plt.plot(dates, highs,c='red', alpha=0.5)
+    plt.plot(dates, lows, c='blue',alpha=0.5)
+    plt.fill_between(dates, highs, lows, facecolor='green', alpha=0.2)
+    # Formata o gráfico
+    title = 'Daily high and low temperatures - 2014\nDeath Valley, CA'
+    plt.title(title, fontsize=20)
+    plt.xlabel('', fontsize = 16)
+    fig.autofmt_xdate()
+    plt.ylabel('Temprature (F)',fontsize=16)
+    plt.tick_params(axis='both', which='major',labelsize=16)
+    plt.ylim(0, 100)
+    plt.savefig('sitka_weather_2014.png', bbox_inches='tight')
     plt.show()
