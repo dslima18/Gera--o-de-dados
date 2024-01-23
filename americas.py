6add977d1e6975d7d12c8431056459d6cfd4c948
Donato Souza <donatosouza33@gmail.com>
2024-01-22 22:50:23 -0300

    Atualização do README

--- americas.py
+++ americas.py
@@ -1,4 +1,4 @@
-import pygal.maps.world
+import pygal
 
 wm = pygal.maps.world.World()
 wm.title  = 'North, Central, and South America'
--- world_population.py
+++ world_population.py
@@ -1,17 +1,27 @@
 import json
+from pygal.maps.world import World
 from country_codes import get_country_code
 # Carrega os dados em uma lista
 filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
-    # Exibe a população de cada país em 2010
+    # constrói um dicionário com dados das populações
+    cc_populations = {}
     for pop_dict in pop_data:
-        if pop_dict['Year'] == '2010':
+        if pop_dict['Year'] == '2010' and 'Value' in pop_dict:
             country_name = pop_dict['Country Name']
             population = int(float(pop_dict['Value']))
             code = get_country_code(country_name)
             if code:
-                print(code + ": " + str(population))
+                cc_populations[code] = population
             else:
-                print('ERROR - '+ country_name)
+                print('Código não encontrado para:', country_name)
+    
+    wm = World()
+    wm.force_uri_protocol = 'http'
+    wm.title = 'World Population in 2010, by Country'
+    wm.add('2010', cc_populations)
+    wm.render_to_file('world_population.svg')
+        
+                #print('ERROR - '+ country_name)
             #print(country_name + ": " + str(population))
